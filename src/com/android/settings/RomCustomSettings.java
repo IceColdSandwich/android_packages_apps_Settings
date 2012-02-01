5d6502e0ae91a56cfe60e80376fe007ee1f29a4c
Davor Bertovic <davor@losinj.com>
2012-02-01 23:13:29 +0100

    add clock customization options

--- src/com/android/settings/RomCustomSettings.java
+++ src/com/android/settings/RomCustomSettings.java
@@ -23,12 +23,29 @@ public class RomCustomSettings extends SettingsPreferenceFragment implements OnP
     private static final String PREF_VOLUME_WAKE = "volume_wake";
     CheckBoxPreference mVolumeWake;
 
+    private static final String PREF_CLOCK_DISPLAY_STYLE = "clock_am_pm";
+    private static final String PREF_CLOCK_STYLE = "clock_style";
+    private ListPreference mAmPmStyle;
+    private ListPreference mClockStyle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.rom_custom_settings);
         PreferenceScreen prefSet = getPreferenceScreen();
+
+        mClockStyle = (ListPreference) prefSet.findPreference(PREF_CLOCK_STYLE);
+        mAmPmStyle = (ListPreference) prefSet.findPreference(PREF_CLOCK_DISPLAY_STYLE);
+
+        int styleValue = Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_AM_PM, 2);
+        mAmPmStyle.setValueIndex(styleValue);
+        mAmPmStyle.setOnPreferenceChangeListener(this);
+
+        int clockVal = Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_CLOCK, 1);
+        mClockStyle.setValueIndex(clockVal);
+        mClockStyle.setOnPreferenceChangeListener(this);
 
         mVolumeWake = (CheckBoxPreference) findPreference(PREF_VOLUME_WAKE);
         mVolumeWake.setChecked(Settings.System.getInt(getActivity()
@@ -50,6 +67,17 @@ public class RomCustomSettings extends SettingsPreferenceFragment implements OnP
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mAmPmStyle) {
+            int statusBarAmPm = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                Settings.System.STATUS_BAR_AM_PM, statusBarAmPm);
+            return true;
+        } else if (preference == mClockStyle) {
+            int val = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                Settings.System.STATUS_BAR_CLOCK, val);
+            return true;
+	}
         return false;
     }
 

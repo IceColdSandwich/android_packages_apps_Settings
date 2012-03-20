ac1730f63cfcf3cd7e1acd843ffa51fb0f1fa0a5
LorD ClockaN <davor@losinj.com>
2012-03-20 15:28:27 +0100

    Apn settings fixes / code maintainence
    Depends on : http://review.cyanogenmod.com/#change,13743
    
    Cleanup unecessary resources and use global ones
      - This fixes the duplicate id issue currently causing apn's to be un-editable
    
    Bring the ui aspect of the preference up to speed with ics
     - Use ics highlight / layout...etc
     - Use fragments
    
    Patchset 5 : Remove theme for ApnEditor
                 Use static access for variables
    Patchset 6 : Launch apn editor internally as a fragment
    	     Replace deprecated managedQuery with CursorLoader
    
    Change-Id: I069415b29846f30665ce76e52f04115d1b65e455
    
    Conflicts:
    
    	AndroidManifest.xml
    	src/com/android/settings/Settings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -603,4 +603,6 @@ public class Settings extends PreferenceActivity implements ButtonBarHandler {
     public static class AdvancedWifiSettingsActivity extends Settings { /* empty */ }
     public static class TextToSpeechSettingsActivity extends Settings { /* empty */ }
     public static class AndroidBeamSettingsActivity extends Settings { /* empty */ }
+    public static class ApnSettingsActivity extends Settings { /* empty */ }
+    public static class ApnEditorActivity extends Settings { /* empty */ }
 }

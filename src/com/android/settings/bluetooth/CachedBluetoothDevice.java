30d3a9b3686c43211201be914acf2a39b82e48c2
Christophe Bransiec <christophex.bransiec@intel.com>
2012-07-06 10:10:11 +0200

    BT: fixes crash caused by unpair/pair in a short time
    Disabling the device in the paired list to avoid
    having the possibility to reconnect the device
    during the unpairing process
    
    Change-Id: I2fa87f921df576926379d931860c7b1ced051472
    Author: Christophe Bransiec <christophex.bransiec@intel.com>
    Signed-off-by: Xiaokang Qin <xiaokang.qin@intel.com>
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
    Signed-off-by: Jack Ren <jack.ren@intel.com>
    Author-tracking-BZ: 43678

--- src/com/android/settings/bluetooth/CachedBluetoothDevice.java
+++ src/com/android/settings/bluetooth/CachedBluetoothDevice.java
@@ -51,6 +51,8 @@ final class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> {
     private BluetoothClass mBtClass;
     private HashMap<LocalBluetoothProfile, Integer> mProfileConnectionState;
 
+    private boolean mUnBonding = false;
+
     private final List<LocalBluetoothProfile> mProfiles =
             new ArrayList<LocalBluetoothProfile>();
 
@@ -146,6 +148,7 @@ final class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> {
                           LocalBluetoothAdapter adapter,
                           LocalBluetoothProfileManager profileManager,
                           BluetoothDevice device) {
+        mUnBonding = false;
         mContext = context;
         mLocalAdapter = adapter;
         mProfileManager = profileManager;
@@ -255,6 +258,7 @@ final class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> {
 
     private boolean ensurePaired() {
         if (getBondState() == BluetoothDevice.BOND_NONE) {
+            mUnBonding = false;
             startPairing();
             return false;
         } else {
@@ -285,6 +289,8 @@ final class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> {
     }
 
     void unpair() {
+        mUnBonding = true;
+
         disconnect();
 
         int state = getBondState();
@@ -425,6 +431,9 @@ final class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> {
     }
 
     boolean isBusy() {
+        if(mUnBonding)
+            return true;
+
         for (LocalBluetoothProfile profile : mProfiles) {
             int status = getProfileConnectionState(profile);
             if (status == BluetoothProfile.STATE_CONNECTING
@@ -498,6 +507,7 @@ final class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> {
 
     void onBondingStateChanged(int bondState) {
         if (bondState == BluetoothDevice.BOND_NONE) {
+            mUnBonding = false;
             mProfiles.clear();
             mConnectAfterPairing = false;  // cancel auto-connect
             setPhonebookPermissionChoice(PHONEBOOK_ACCESS_UNKNOWN);

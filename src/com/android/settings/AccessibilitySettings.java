1a656e6c015485071154fd853b654e5fe1a04c87
LorDClockaN <davor@losinj.com>
2012-08-31 20:36:44 +0200

    Allow answering incomming call with HOME button (in case of hardware HOME buttom it is useful during winter time)
    Patch set 2: Dutch translations added
    
    Change-Id: I7958a4ea040b1f7969e53de34523db3d8ec46ec0
    
    Conflicts:
    
    	res/values-nl/strings.xml

--- src/com/android/settings/AccessibilitySettings.java
+++ src/com/android/settings/AccessibilitySettings.java
@@ -106,6 +106,8 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
     private static final String TOGGLE_LARGE_TEXT_PREFERENCE = "toggle_large_text_preference";
     private static final String TOGGLE_POWER_BUTTON_ENDS_CALL_PREFERENCE =
         "toggle_power_button_ends_call_preference";
+    private static final String TOGGLE_HOME_BUTTON_ANSWERS_CALL_PREFERENCE =
+        "toggle_home_button_answers_call_preference";
     private static final String TOGGLE_AUTO_ROTATE_SCREEN_PREFERENCE =
         "toggle_auto_rotate_screen_preference";
     private static final String TOGGLE_SPEAK_PASSWORD_PREFERENCE =
@@ -160,6 +162,7 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
 
     private CheckBoxPreference mToggleLargeTextPreference;
     private CheckBoxPreference mTogglePowerButtonEndsCallPreference;
+    private CheckBoxPreference mToggleHomeButtonAnswersCallPreference;
     private CheckBoxPreference mToggleAutoRotateScreenPreference;
     private CheckBoxPreference mToggleSpeakPasswordPreference;
     private Preference mToggleTouchExplorationPreference;
@@ -213,6 +216,9 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
         } else if (mTogglePowerButtonEndsCallPreference == preference) {
             handleTogglePowerButtonEndsCallPreferenceClick();
             return true;
+        } else if (mToggleHomeButtonAnswersCallPreference == preference) {
+            handleToggleHomeButtonAnswersCallPreferenceClick();
+            return true;
         } else if (mToggleAutoRotateScreenPreference == preference) {
             handleToggleAutoRotateScreenPreferenceClick();
             return true;
@@ -237,6 +243,14 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
                 (mTogglePowerButtonEndsCallPreference.isChecked()
                         ? Settings.Secure.INCALL_POWER_BUTTON_BEHAVIOR_HANGUP
                         : Settings.Secure.INCALL_POWER_BUTTON_BEHAVIOR_SCREEN_OFF));
+    }
+
+    private void handleToggleHomeButtonAnswersCallPreferenceClick() {
+        Settings.Secure.putInt(getContentResolver(),
+                Settings.Secure.RING_HOME_BUTTON_BEHAVIOR,
+                (mToggleHomeButtonAnswersCallPreference.isChecked()
+                        ? Settings.Secure.RING_HOME_BUTTON_BEHAVIOR_ANSWER
+                        : Settings.Secure.RING_HOME_BUTTON_BEHAVIOR_DO_NOTHING));
     }
 
     private void handleToggleAutoRotateScreenPreferenceClick() {
@@ -273,6 +287,14 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
         if (!KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_POWER)
                 || !Utils.isVoiceCapable(getActivity())) {
             mSystemsCategory.removePreference(mTogglePowerButtonEndsCallPreference);
+        }
+
+        // Home button answers calls.
+        mToggleHomeButtonAnswersCallPreference =
+            (CheckBoxPreference) findPreference(TOGGLE_HOME_BUTTON_ANSWERS_CALL_PREFERENCE);
+        if (!KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_HOME)
+                || !Utils.isVoiceCapable(getActivity())) {
+            mSystemsCategory.removePreference(mToggleHomeButtonAnswersCallPreference);
         }
 
         // Auto-rotate screen
@@ -435,6 +457,17 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
             final boolean powerButtonEndsCall =
                 (incallPowerBehavior == Settings.Secure.INCALL_POWER_BUTTON_BEHAVIOR_HANGUP);
             mTogglePowerButtonEndsCallPreference.setChecked(powerButtonEndsCall);
+        }
+
+        // Home button answers calls.
+        if (KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_HOME)
+                && Utils.isVoiceCapable(getActivity())) {
+            final int incallHomeBehavior = Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.RING_HOME_BUTTON_BEHAVIOR,
+                    Settings.Secure.RING_HOME_BUTTON_BEHAVIOR_DEFAULT);
+            final boolean homeButtonAnswersCall =
+                (incallHomeBehavior == Settings.Secure.RING_HOME_BUTTON_BEHAVIOR_ANSWER);
+            mToggleHomeButtonAnswersCallPreference.setChecked(homeButtonAnswersCall);
         }
 
         // Auto-rotate screen
